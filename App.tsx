c7209c9e6078511634df06626cd51a25b7f78459
= <daniell.felipe.2017@gmail.com>
2025-03-16 18:15:47 -0300

    add roboto font and loading indicator

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
 import { ThemeProvider } from 'styled-components'
 
+import { Loading } from '@/components/Loading'
 import { Groups } from '@/screens/Groups'
 import theme from '@/theme'
 
 export default function App() {
+  const [isFontLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {isFontLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   )
 }
