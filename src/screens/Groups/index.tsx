e002287fce57dd5ff306ad49c339dcdf521573be
= <daniell.felipe.2017@gmail.com>
2025-03-17 21:02:35 -0300

    create Button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,12 @@
-import { Header } from '@/components/Header'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 
+import { Header } from '@/components/Header'
 import { Highlight } from '@/components/Highlight'
 import { Container } from './styles'
 import { GroupCard } from '@/components/GroupCard'
-import { useState } from 'react'
-import { FlatList } from 'react-native'
 import { ListEmpty } from '@/components/ListEmpty'
+import { Button } from '@/components/Button'
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
@@ -25,6 +26,8 @@ export function Groups() {
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
+
+      <Button title="Criar nova turma" />
     </Container>
   )
 }
