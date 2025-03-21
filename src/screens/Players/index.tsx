d726eeb5255cda1d3b5249f35f1ed749b5fac7d0
= <daniell.felipe.2017@gmail.com>
2025-03-20 21:45:43 -0300

    add team list

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,17 @@
+import { useState } from 'react'
+
 import { Header } from '@/components/Header'
-import { Container, Form } from './styles'
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles'
 import { Highlight } from '@/components/Highlight'
 import { ButtonIcon } from '@/components/ButtonIcon'
 import { Input } from '@/components/Input'
 import { Filter } from '@/components/Filter'
+import { FlatList } from 'react-native'
 
 export function Players() {
+  const [team, setTeam] = useState('Time A')
+  const [players, setPlayers] = useState([])
+
   return (
     <Container>
       <Header showBackButton />
@@ -20,7 +26,22 @@ export function Players() {
         <ButtonIcon icon="add" />
       </Form>
 
-      <Filter title="Time A" isActive />
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
   )
 }
