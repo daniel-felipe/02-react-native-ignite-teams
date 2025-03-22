d9245dcc6b16c93938722f15cfa5c97de949be7d
= <daniell.felipe.2017@gmail.com>
2025-03-22 18:36:57 -0300

    add navigation to Header and NewGroup

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,9 +3,16 @@ import { Header } from '@/components/Header'
 import { Highlight } from '@/components/Highlight'
 import { Input } from '@/components/Input'
 
+import { useNavigation } from '@react-navigation/native'
 import { Container, Content, Icon } from './styles'
 
 export function NewGroup() {
+  const navigation = useNavigation()
+
+  function handleNew() {
+    navigation.navigate('players', { group: 'Rocket' })
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -16,7 +23,8 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar as pessoas"
         />
         <Input placeholder="Nome da turma" />
-        <Button title="Criar" style={{ marginTop: 20 }} />
+
+        <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
   )
